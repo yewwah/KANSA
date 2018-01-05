064cabb6fe628f4165499d416c12ec9beced5dfa
yewwah <yewwah10@gmail.com>
2018-01-06 00:10:03 +0800

    modified to allow for loading files into their relevant data types

--- process_kansa.py
+++ process_kansa.py
@@ -6,45 +6,37 @@ import traceback
 
 
 class Process():
-    def __init__(self, input_fol = 'data', output_fol = 'data_processed'):
+    def __init__(self, input_fol='data', output_fol='data_processed'):
         self.input_folder = input_fol
         self.output_folder = output_fol
 
+    def process_kansa_report(self, input_folder='data'):
+        data_folders = os.listdir(input_folder)
+        for data_folder in data_folders:
+            for root, dir, fil in os.walk(input_folder + os.sep + data_folder):
+                # Only process the first fil
+                fname = fil[0]
 
-    def process_kansa_report(self, input_folder = 'data'):
-        data_files = os.listdir(input_folder)
-        for data in data_files:
+                # Skip the log file
+                if not fil or '.Log' in fname:
+                    continue
 
-            # Creates the processed folder if it does not exists
-            try:
-                df, pc_name = self._process_data(data)
-            except:
-                print data
+                try:
+                    # splits the files into folder corresponding to each data type
+                    pc_name, datatype = fname.replace('.csv', '').rsplit('-', 1)
+                    df = self._read_csv(root + os.sep + fname, pc_name)
 
-            processed_path = self.output_folder + os.sep + pc_name + '_processed'
-            if not os.path.exists(processed_path):
-                os.mkdir(processed_path)
-
-    def _process_data(self, data_folder):
-        """
-        :param data_folder:
-        Folder which contains the following structure
-        Original Folder
-            - AMhealthstatus
-                -AMHealthStatus.csv
-
-        :return:
-        Dataframe: A processed dataframe
-        directory : str representation of the name of the folder
-        """
-        for root, dir, fil in os.walk(data_folder):
-            for directory in dir:
-                for fname in os.listdir(root + os.sep + directory):
-                    abs_path = root + os.sep + directory + os.sep + fname
-                    # pc_name format is NAME-datatype
-                    pc_name = fname.rsplit('-', 1)[0]
-                    dataframe = self._read_csv(abs_path, pc_name)
-                    return dataframe , directory
+                    # Create the processed folder if it does not exists
+                    processed_path = self.output_folder + os.sep + datatype + '_processed'
+                    if not os.path.exists(processed_path):
+                        os.mkdir(processed_path)
+                    if not df.empty:
+                        df.to_csv(processed_path + os.sep + pc_name + '.csv', encoding='UTF-8', index=False)
+                except:
+                    print fil
+                    traceback.print_exc()
+                    exit(0)
+                    print 'here'
 
     def _read_csv(self, abs_path, pc_name):
         """
@@ -58,7 +50,7 @@ class Process():
             first_line = f.readline()
             # \xff\xfe is a null byte
             if not first_line or first_line == '\xff\xfe':
-                return None
+                return pd.DataFrame()
 
         # Detects encoding
         rawdata = open(abs_path, 'rb').read()
@@ -68,18 +60,19 @@ class Process():
         # Reads the csv
         try:
             df = pd.read_csv(abs_path, encoding=charenc)
-            df['header'] = pc_name
+            df['PC_name'] = pc_name
 
             # Appends the pc name to the start of a column
-            df.reindex(columns=['n'] + df.columns[:-1].tolist())
+            df = df.reindex(columns=['PC_name'] + df.columns[:-1].tolist())
             return df
 
         except CParserError:
             print '{} failed to parse'.format(abs_path)
         except:
             traceback.print_exc()
-        return None
 
-        # _read_csv('Output_20180104160930\ClrVersion\DESKTOP-GPAN1LT-ClrVersion.csv', 'a')
+        return pd.DataFrame()
+
+
 a = Process()
 a.process_kansa_report()
